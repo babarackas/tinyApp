50bfcb7e1f1c2868c373d51e8999479c76d9d862
Philip <pbazzard@gmail.com>
2017-03-15 03:23:12 +0000

    added random alphanumeric generator function and form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//allow us to access POST request parameters,
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set("view engine", "ejs");
 
 
@@ -10,12 +15,29 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//random string generator
+function generateRandomString() {
+  var text = " ";
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 6; i++ )
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+      return text;
+    }
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", {
-    testing: urlDatabase
-  });
+  //let longURL need to use this later
+  //console.log(req.params.id);
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
