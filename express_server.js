1a1e563a87e22ffc30053622ddb2150d178ab819
Philip <pbazzard@gmail.com>
2017-03-15 05:09:45 +0000

    added redirect after form submission but not currently working

--- express_server.js
+++ express_server.js
@@ -9,13 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-//random string generator
+//random string generator of 6 numbers and letters
 function generateRandomString() {
   var text = " ";
   var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -31,30 +29,33 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  //let longURL need to use this later
-  //console.log(req.params.id);
+  let templateVars = { shortURL: req.params.id,
+    link: urlDatabase[req.params.id]
+   };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+ let longURL = urlDatabase[req.params.shortURL];
+ console.log(longURL);
+ res.redirect(longURL);
+});
+
+//connection to home page
+app.get("/", (req, res) => {
+  console.log(urlDatabase);
+  var test = "HELLO PIP";
+  res.render("urls_index", {
+    testing: urlDatabase
+  });
+});
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ urlDatabase[random] = req.body.longURL;
+ var newURL = "/urls/" + random;
+ res.redirect(newURL);
 });
 
-
-// app.get("/", (req, res) => {
-//   console.log(urlDatabase);
-//   var test = "HELLO PIP";
-//   res.render("urls_index", {
-//     testing: urlDatabase
-//   });
-// });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
